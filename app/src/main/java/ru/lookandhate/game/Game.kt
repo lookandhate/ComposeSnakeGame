47f9574faef6b41667555f128914d72dca559388
lookandhate <princessselestiapwnz@mail.ru>
2022-11-02 10:23:49 +0400

    Moved gamestate related classes to different file

---
+++ app/src/main/java/ru/lookandhate/game/Game.kt
@@ -0,0 +1,107 @@
+package ru.lookandhate.game
+
+import androidx.room.Room
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
+import ru.lookandhate.game.Room.AppDataBase
+import ru.lookandhate.game.Room.GameResult
+import kotlin.random.Random
+
+data class State(
+    val food: Pair<Int, Int>,
+    val snake: List<Pair<Int, Int>>,
+    var points: Int,
+    var snakeLength: Int = 4
+)
+
+class Game(val scope: CoroutineScope, context: MainActivity) {
+    private val mutableState: MutableStateFlow<State> =
+        MutableStateFlow(State(Pair(8, 8), listOf(Pair(0, 0)), 0))
+    val state: Flow<State> = mutableState
+    private val mutex = Mutex()
+
+    private val db = Room.databaseBuilder(context, AppDataBase::class.java, "database")
+        .build()
+
+    var move = Pair(0, 1)
+        set(value) {
+            scope.launch {
+                mutex.withLock {
+                    field = value
+                }
+            }
+        }
+
+    private suspend fun loseGame(gameState: State) {
+        val gameResultDao = db.gameResultDao()
+
+        gameResultDao.insert(
+            GameResult(
+                points = gameState.points,
+                date = System.currentTimeMillis()
+            )
+        )
+        gameState.snakeLength = 4
+        gameState.points = 0
+    }
+
+    private fun eatFood(gameState: State) {
+        gameState.snakeLength++
+        gameState.points++
+    }
+
+    private suspend fun getNewPosition(gameState: State): Pair<Int, Int> {
+        val newPosition = gameState.snake.first().let { pos ->
+            mutex.withLock {
+                Pair(
+                    (pos.first + move.first + BOARD_SIZE) % BOARD_SIZE,
+                    (pos.second + move.second + BOARD_SIZE) % BOARD_SIZE
+                )
+            }
+        }
+        return newPosition
+    }
+
+
+    companion object {
+        const val BOARD_SIZE = 16
+    }
+
+
+    init {
+        scope.launch {
+            while (true) {
+                delay(150)
+                mutableState.update {
+                    val newPosition = getNewPosition(it)
+                    if (newPosition == it.food) {
+                        eatFood(it)
+                    }
+
+                    if (it.snake.contains(newPosition)) {
+                        loseGame(it)
+                    }
+
+                    it.copy(
+                        food = if (newPosition == it.food) {
+                            Pair(
+                                (Random.nextInt(BOARD_SIZE)),
+                                (Random.nextInt(BOARD_SIZE))
+                            )
+                        } else {
+                            it.food
+                        },
+                        snake = listOf(newPosition) + it.snake.take(it.snakeLength - 1)
+                    )
+                }
+            }
+        }
+    }
+
+}
--- app/src/main/java/ru/lookandhate/game/MainActivity.kt
+++ app/src/main/java/ru/lookandhate/game/MainActivity.kt
@@ -36,97 +36,7 @@ import ru.lookandhate.game.Room.GameResultDao
 import ru.lookandhate.game.ui.theme.GameTheme
 import kotlin.random.Random
 
-data class State(
-    val food: Pair<Int, Int>,
-    val snake: List<Pair<Int, Int>>,
-    var points: Int,
-    var snakeLength: Int = 4
-)
 
-class Game(val scope: CoroutineScope, context: MainActivity) {
-    private val mutableState: MutableStateFlow<State> =
-        MutableStateFlow(State(Pair(8, 8), listOf(Pair(0, 0)), 0))
-    val state: Flow<State> = mutableState
-    private val mutex = Mutex()
-    private val db = Room.databaseBuilder(context, AppDataBase::class.java, "database")
-        .build()
-
-    var move = Pair(0, 1)
-        set(value) {
-            scope.launch {
-                mutex.withLock {
-                    field = value
-                }
-            }
-        }
-
-    private suspend fun loseGame(gameState: State) {
-        val gameResultDao = db.gameResultDao()
-
-        gameResultDao.insert(
-            GameResult(
-                points = gameState.points,
-                date = System.currentTimeMillis()
-            )
-        )
-        gameState.snakeLength = 4
-        gameState.points = 0
-    }
-
-    private fun eatFood(gameState: State) {
-        gameState.snakeLength++
-        gameState.points++
-    }
-
-    private suspend fun getNewPosition(gameState: State): Pair<Int, Int> {
-        val newPosition = gameState.snake.first().let { pos ->
-            mutex.withLock {
-                Pair(
-                    (pos.first + move.first + BOARD_SIZE) % BOARD_SIZE,
-                    (pos.second + move.second + BOARD_SIZE) % BOARD_SIZE
-                )
-            }
-        }
-        return newPosition
-    }
-
-
-    companion object {
-        const val BOARD_SIZE = 16
-    }
-
-
-    init {
-        scope.launch {
-            while (true) {
-                delay(150)
-                mutableState.update {
-                    val newPosition = getNewPosition(it)
-                    if (newPosition == it.food) {
-                        eatFood(it)
-                    }
-
-                    if (it.snake.contains(newPosition)) {
-                        loseGame(it)
-                    }
-
-                    it.copy(
-                        food = if (newPosition == it.food) {
-                            Pair(
-                                (Random.nextInt(BOARD_SIZE)),
-                                (Random.nextInt(BOARD_SIZE))
-                            )
-                        } else {
-                            it.food
-                        },
-                        snake = listOf(newPosition) + it.snake.take(it.snakeLength - 1)
-                    )
-                }
-            }
-        }
-    }
-
-}
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -143,19 +53,6 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    GameTheme {
-        Greeting("Android")
     }
 }
 
