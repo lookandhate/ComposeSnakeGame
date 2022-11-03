a9d32d8e416095ff9f7bd53ac27d2c145cb74106
lookandhate <princessselestiapwnz@mail.ru>
2022-11-04 01:15:42 +0400

    Added sorting for points and date

--- app/src/main/java/ru/lookandhate/game/Screens/RecordsScreen.kt
+++ app/src/main/java/ru/lookandhate/game/Screens/RecordsScreen.kt
@@ -6,34 +6,44 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.text.ClickableText
 import androidx.compose.material.Divider
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.unit.dp
 import ru.lookandhate.game.Room.GameResult
 import java.util.*
 
 @Composable
 fun RecordScreen(records: List<GameResult>) {
-//    Text(text = "Records")
+    val recordsState = remember { mutableStateListOf<GameResult>() }
+    records.forEach { recordsState.add(it) }
     Log.d("Records", records.toString())
     LazyColumn {
         item() {
             Row(
             ) {
                 Column(modifier = Modifier.weight(0.7f)) {
-                    Text(text = "Date of record")
+                    ClickableText(AnnotatedString("Date of record"), onClick = {
+                        recordsState.sortByDescending { it.date }
+                    })
                 }
                 Column( Modifier.weight(0.3f)) {
-                    Text(text = "Points")
+                    ClickableText(AnnotatedString("Points"), onClick = {
+                        recordsState.sortByDescending { it.points }
+
+                    })
                 }
 
             }
             Divider()
         }
 
-        items(records) { record ->
+        items(recordsState) { record ->
             RecordItem(record)
 
         }
