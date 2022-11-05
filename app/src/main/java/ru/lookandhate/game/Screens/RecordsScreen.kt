4c06d7402924d63fb69809c4620fa30981a260a9
lookandhate <princessselestiapwnz@mail.ru>
2022-11-05 15:42:24 +0400

    Beautified table header

--- app/src/main/java/ru/lookandhate/game/Screens/RecordsScreen.kt
+++ app/src/main/java/ru/lookandhate/game/Screens/RecordsScreen.kt
@@ -1,9 +1,7 @@
 package ru.lookandhate.game.Screens
 
 import android.util.Log
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.text.ClickableText
@@ -13,8 +11,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import ru.lookandhate.game.Room.GameResult
 import java.util.*
 
@@ -23,17 +24,22 @@ fun RecordScreen(records: List<GameResult>) {
     val recordsState = remember { mutableStateListOf<GameResult>() }
     records.forEach { recordsState.add(it) }
     Log.d("Records", records.toString())
-    LazyColumn {
+    LazyColumn(modifier = Modifier.padding(16.dp)) {
         item() {
             Row(
             ) {
                 Column(modifier = Modifier.weight(0.7f)) {
-                    ClickableText(AnnotatedString("Date of record"), onClick = {
-                        recordsState.sortByDescending { it.date }
-                    })
+                    ClickableText(
+                        text = AnnotatedString("Date of record"),
+                        style= TextStyle(fontSize = 20.sp, color = Color.Blue),
+                        onClick = {
+                            recordsState.sortByDescending { it.date }
+                        })
                 }
-                Column( Modifier.weight(0.3f)) {
-                    ClickableText(AnnotatedString("Points"), onClick = {
+                Column(modifier = Modifier.weight(0.3f)) {
+                    ClickableText(text=AnnotatedString("Points"),
+                        style= TextStyle(fontSize = 20.sp, color = Color.Blue),
+                        onClick = {
                         recordsState.sortByDescending { it.points }
 
                     })
@@ -57,7 +63,7 @@ fun RecordItem(record: GameResult) {
         Column(modifier = Modifier.weight(0.7f)) {
             Text(text = Date(record.date).toString())
         }
-        Column( Modifier.weight(0.3f)) {
+        Column(Modifier.weight(0.3f)) {
             Text(text = record.points.toString())
         }
 
